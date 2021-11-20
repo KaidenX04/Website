126d632408577a85479ac3f8f200f3d1ae1bfc66
KaidenX04 <kaidenflaig335@gmail.com>
2021-11-20 01:20:31 -0700

    changed ALOT

--- Pages/calculator/calculator.js
+++ Pages/calculator/calculator.js
@@ -1,43 +1,41 @@
 function Calculator() {
-    const cont = 1
 
-    while (cont === 1) {
+    let a = Number(document.getElementById("firstNumberInput").value);
+    let b = String(document.getElementById("operatorNumberInput").value);
+    let c = Number(document.getElementById("secondNumberInput").value);
 
-        let exit_or_stay = (Number(prompt("Press 1 to Calculate\r" +
-            "Press 2 to exit")));
-
-        if (exit_or_stay === 1) {
-
-            let a = (Number(prompt("Input First Number")));
-            let b = prompt("Input Operator");
-
-            if (b === "+" || "-" || "x" || "X" || "*" || "/") {
-
-                let c = (Number(prompt("Input Second Number")));
-
-                if (b === "+") {
-                    let answer = (a + c);
-                    alert(answer)
-                } else if (b === "-") {
-                    let answer = (a - c);
-                    alert(answer)
-                } else if (b === "/" || "div" || "Div") {
-                    let answer = (a / c);
-                    alert(answer)
-                } else if (b === "x" || "*" || "X") {
-                    let answer = (a * c);
-                    alert(answer)
-                } else {
-                    alert("Invalid input")
-                }
-            } else {
-                alert("Invalid operator")
-            }
-        } else if (exit_or_stay === 2) {
-            return
-        } else {
-            alert("Invalid input")
+    switch(b)
+    {
+        case"+": {
+            let answer = (a + c);
+            alert(answer)
+            break
+        }
+        case"-": {
+            let answer = (a - c);
+            alert(answer)
+            break
+        }
+        case "/":  case"div":  case"Div": {
+            let answer = (a / c);
+            alert(answer)
+            break
+        }
+        case "x": case "*": case "X": {
+            let answer = (a * c);
+            alert(answer)
+            break
         }
 
+        default: {
+            alert("invalid Operator")
+            break
+        }
     }
 }
+
+
+
+
+
+
--- Pages/temperatureConverter/temperature_converter.js
+++ Pages/temperatureConverter/temperature_converter.js
@@ -1,17 +1,12 @@
-function tempConverter() {
-    let temp_c = (document.getElementById("temperatureInput").value);
+function celsiusConverter() {
+    let temp_c = (document.getElementById("celsiusInput").value);
     let temp_f = (temp_c * 9 / 5 + 32).toFixed(2);
     alert("That is " + temp_f + " " + "Degrees Fahrenheit");
 }
 
-
-
-
-
-            //else if (choose_celsius_or_fahrenheit === 2) {
-            //let temp_f = Number(prompt("Enter Temperature in Fahrenheit"));
-            //let temp_c = ((temp_f - 32) * 9 / 5).toFixed(2);
-            //alert("That is " + temp_c + " " + "Degrees Celsius");
-
-
+function fahrenheitConverter() {
+    let temp_f = (document.getElementById("fahrenheitInput").value);
+    let temp_c = ((temp_f - 32) * 9 / 5).toFixed(2);
+    alert("That is " + temp_c + " " + "Degrees Celsius");
+}
 
